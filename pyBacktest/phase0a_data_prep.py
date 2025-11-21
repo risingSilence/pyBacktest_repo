e6a72700341107ce3e5585616db92ee1f7c5b0aa
Rising Silence <risingsilencegaming@gmail.com>
2025-11-21 17:47:06 +0100

    Test change on macos

--- pyBacktest/phase0a_data_prep.py
+++ pyBacktest/phase0a_data_prep.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import pandas as pd
 
 # ---------------------------------
-# CONFIG
+# CONFIGURATION
 # ---------------------------------
 
 # Ordner, in dem deine M1-CSV-Dateien liegen
